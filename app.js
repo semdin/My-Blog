d3ed0f11dd6e2d5233750194e34f71ae1a84d5d1
semdin <mehmetsemdinaktay@gmail.com>
2023-03-06 19:36:00 +0300

    adding database and changing settings for cyclic

--- app.js
+++ app.js
@@ -4,11 +4,28 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
+mongoose.connect("mongodb+srv://mehmetsemdinaktay:8e5GaYlmmOW8XD3y@cluster0.huw09px.mongodb.net/blogsDB", {useNewUrlParser: true});
+
+
+const composeSchema = {
+  title : String,
+  content : String
+};
+
+const Compose = new mongoose.model("Compose", composeSchema);
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const composeDefault = new Compose(
+  {
+    title: "Home",
+    content: homeStartingContent
+  }
+);
 
 const app = express();
 const posts = [];
@@ -19,9 +36,12 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-  res.render('home', {
-    home: homeStartingContent,
-    posts: posts
+
+  Compose.find({}).then(function(foundItems){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: foundItems
+      });
   });
 });
 app.get("/about", function(req, res){
@@ -34,33 +54,27 @@ app.get("/compose", function(req, res){
   res.render('compose');
 });
 
-app.get('/posts/:postName', (req, res) => {
+app.get("/posts/:postId", function(req, res){
 
-  posts.forEach(function(post){
-    if(_.lowerCase(post.title) === _.lowerCase(req.params.postName)){
-      console.log("Match Found!");
-      res.render('post', {
+  const requestedPostId = req.params.postId;
+  
+    Compose.findOne({_id: requestedPostId}).then(function(post){
+      res.render("post", {
         title: post.title,
         content: post.content
       });
-
-    }
-    else{
-      console.log("Match Not Found!");
-    }
+    });
+  
   });
-
-  console.log(_.lowerCase(req.params.postName));
-});
 
 
 app.post("/compose", function(req,res){
-  const post = {
+  const post = new Compose({
     title : req.body.postTitle,
     content : req.body.postContent
-  };
+  });
+  post.save();
   res.redirect("/");
-  posts.push(post);
 });
 
 
